350fd39c5dd2d46c7edd8b6aec6cf40a1829e771
Magnus Farstad <magnus.r.farstad@outlook.com>
2022-04-21 10:03:18 +0200

    Created BaseSearchBar for homepage

--- src/components/HomeComponent.vue
+++ src/components/HomeComponent.vue
@@ -1,10 +1,25 @@
 <template>
   <div class="welcomeMessage">Din side for å leie!</div>
+  <form class="form" @submit.prevent="submit">
+    <BaseSearchBar :label="'Søk...'"></BaseSearchBar>
+    <button class="searchButton">
+      <img src="@/assets/hamburger.svg" alt="" />
+    </button>
+  </form>
 </template>
 
 <script>
+import BaseSearchBar from "@/components/baseTools/BaseSearchBar";
 export default {
   name: "HomeComponent",
+  components: {
+    BaseSearchBar,
+  },
+  methods: {
+    submit() {
+      console.log("submitted");
+    },
+  },
 };
 </script>
 
@@ -28,5 +43,18 @@ a {
 .welcomeMessage {
   font-size: 40px;
   padding: 40px 10px;
+  margin: 2rem 0;
+}
+
+.form {
+  display: grid;
+  border-radius: 7px;
+  box-shadow: 0 0 3px;
+  grid-template-columns: 85% 15%;
+  height: 4rem;
+}
+.searchButton {
+  border: none;
+  background: transparent;
 }
 </style>
---
+++ src/components/baseTools/BaseSearchBar.vue
@@ -0,0 +1,47 @@
+<template>
+  <input
+    v-bind="$attrs"
+    :placeholder="label"
+    class="inputField"
+    :value="modelValue"
+    @input="$emit('update:modelValue', $event.target.value)"
+    :id="uuid"
+  />
+</template>
+
+<script>
+import UniqueID from "@/features/UniqueID";
+export default {
+  name: "BaseSearchBar",
+  props: {
+    label: {
+      type: String,
+      default: "",
+    },
+    modelValue: {
+      type: [String, Number],
+      default: "",
+    },
+    error: {
+      type: String,
+      default: "",
+    },
+  },
+  setup() {
+    const uuid = UniqueID().getID();
+
+    return {
+      uuid,
+    };
+  },
+};
+</script>
+
+<style scoped>
+input {
+  padding: 1rem 0 1rem 1.5rem;
+  border: none;
+  border-radius: 10px;
+  font-size: 20px;
+}
+</style>
