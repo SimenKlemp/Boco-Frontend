718a5c965a2e977f15a6e947d06cb3b8a2ce0360
Magnus Farstad <magnus.r.farstad@outlook.com>
2022-04-21 11:06:11 +0200

    Created Item cards files

---
+++ src/components/itemCards/ItemCardHorizontal.vue
@@ -0,0 +1,9 @@
+<template>Hei</template>
+
+<script>
+export default {
+  name: "ItemCardHorizontal",
+};
+</script>
+
+<style scoped></style>
---
+++ src/components/itemCards/ItemCardSquare.vue
@@ -0,0 +1,11 @@
+<template>
+  <v-card>Test</v-card>
+</template>
+
+<script>
+export default {
+  name: "ItemCardSquare",
+};
+</script>
+
+<style scoped></style>
