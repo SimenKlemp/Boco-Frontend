30b366badc39e21abfefbfea9ccd70535a56764a
Ida Green Heglund <idagh@stud.ntnu.no>
2022-04-22 15:43:25 +0200

    added datepicker

--- src/components/baseTools/Datepicker.vue
+++ src/components/baseTools/Datepicker.vue
@@ -1,11 +1,11 @@
 <template>
-  <Datepicker v-model="date" range></Datepicker>
+  <Datepicker class="dp" v-model="date" range></Datepicker>
 </template>
 
 <script>
 import Datepicker from "@vuepic/vue-datepicker";
 import { ref, onMounted } from "vue";
-import "@vuepic/vue-datepicker/dist/main.css";
+import '@vuepic/vue-datepicker/src/VueDatePicker/style/main.scss';
 
 export default {
   components: { Datepicker },
@@ -14,7 +14,6 @@ export default {
 
     // For demo purposes assign range from the current date
     onMounted(() => {
-      console.log("hei");
       const startDate = new Date();
       const endDate = new Date(new Date().setDate(startDate.getDate() + 7));
       date.value = [startDate, endDate];
@@ -26,3 +25,5 @@ export default {
   },
 };
 </script>
+<style scoped>
+</style>
