eeb3b321bc02196e5b8aabb23b9c3e5e803b5b81
Magnus Farstad <magnus.r.farstad@outlook.com>
2022-04-29 15:54:47 +0200

    Created page for viewing all chats

---
+++ src/components/ChatCard.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="chatCardContainer">
+    <div class="itemImageContainer">
+      <img
+        class="itemImage"
+        :src="'http://localhost:8085/api/image/' + rental.item.imageId"
+        alt=""
+      />
+    </div>
+    <div class="itemMeta">
+      <div class="name">{{ rental.item.user.name }}</div>
+      <div class="message">{{ messageWrapped }}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ChatCard",
+  props: {
+    rental: {
+      type: Object,
+      required: true,
+    },
+  },
+  computed: {
+    messageWrapped() {
+      if (this.rental.item.description.length > 40) {
+        return this.rental.item.description.slice(0, 40) + "...";
+      }
+      return this.rental.item.description;
+    },
+  },
+};
+</script>
+
+<style scoped>
+.chatCardContainer {
+  display: flex;
+  margin-bottom: 2rem;
+}
+.itemImageContainer {
+  width: 4.5rem;
+  height: 3.7rem;
+  margin-right: 1.5rem;
+}
+.itemImage {
+  border-radius: 10px;
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+}
+
+.itemMeta {
+  text-align: left;
+}
+.name {
+  font-size: 20px;
+  margin-bottom: 1rem;
+}
+.message {
+  color: #707070;
+}
+</style>
---
+++ src/views/AllChatsView.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="container">
+    <div class="headerContainer">
+      <div class="title">Alle chatter</div>
+    </div>
+    <ChatCard
+      v-for="rental in rentals"
+      :key="rental.rentalId"
+      :rental="rental"
+      @click="goToChat(rental)"
+    ></ChatCard>
+  </div>
+</template>
+
+<script>
+import ChatCard from "@/components/ChatCard";
+export default {
+  name: "AllChatsView",
+  components: {
+    ChatCard,
+  },
+  methods: {
+    goToChat(rental) {
+      this.$store.dispatch("setRental", rental);
+      this.$router.push({ name: "MessageView" });
+    },
+  },
+  computed: {
+    hasProfileImage() {
+      return this.$store.state.userInfo.imageId !== null;
+    },
+    rentals() {
+      return this.$store.state.myRentals;
+    },
+  },
+  mounted() {
+    this.$store.dispatch("fetchMyRentals");
+  },
+};
+</script>
+
+<style scoped>
+.container {
+  padding: 1rem;
+  margin-bottom: 2rem;
+}
+.headerContainer {
+  display: flex;
+  justify-content: center;
+  border-bottom: solid black 1px;
+  margin-bottom: 2rem;
+  padding-bottom: 0.5rem;
+}
+.title {
+  font-size: 20px;
+}
+</style>
