4860f7f85415f952fc30d742cb8f9cbbd7d97a03
frodpede <frodpede@stud.ntnu.no>
2022-05-03 16:26:15 +0200

    Added LoginComponentTest

---
+++ tests/unit/components/LoginComponentTest.spec.js
@@ -0,0 +1,32 @@
+import LoginComponent from "@/components/LoginComponent";
+import {mount} from "@vue/test-utils";
+
+
+describe("LoginComponent", () => {
+    let $store;
+    beforeEach( () => {
+
+        $store = {
+            commit: () => {},
+            dispatch: () => {},
+            state: {
+                userInfo: ""
+            }
+        }
+    });
+    test("If the page renders", () => {
+        const wrapper = mount(LoginComponent, {
+            global: {
+                mocks: {
+                    $store,
+                },
+            },
+        });
+
+        expect(wrapper.login).toMatchSnapshot();
+    });
+    test("When login button is clicked", () =>{
+
+    });
+
+})
