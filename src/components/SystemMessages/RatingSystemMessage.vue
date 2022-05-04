fdd6b1a6d5b115b62de9cbf6c8ef4783424f93d5
Andreas H. Dahle <andreas@dbate.no>
2022-05-04 12:45:34 +0200

    System messages (wip)

---
+++ src/components/SystemMessages/RatingSystemMessage.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="systemMessageContainer">
+    <div id="rateMessage" >
+      <label> Hva synes du om leieforholdet?</label>
+      <div id="ratingLink">
+        Gi <strong>Navn Navnesen</strong> en vurdering
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="20"
+            height="16.473"
+            viewBox="0 0 20 16.473"
+        >
+          <path
+              id="noun-arrow-2094742"
+              d="M19.527,15.758,12.943,9.174a1.612,1.612,0,1,0-2.28,2.278l3.892,3.886H1.612a1.612,1.612,0,0,0,0,3.224H14.519l-3.856,3.862a1.612,1.612,0,0,0,2.28,2.278l6.584-6.584A1.6,1.6,0,0,0,20,16.977a.254.254,0,0,0,0-.04.282.282,0,0,0,0-.04A1.614,1.614,0,0,0,19.527,15.758Z"
+              transform="translate(0 -8.672)"
+              fill="#fb8500"
+          />
+        </svg>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import {getSent} from "@/service/apiService";
+
+export default {
+  name: "RatingSystemMessage",
+  /* methods:{
+ rentalRate() {
+     let response = getSent(this.$store.state.currentRental(), this.$store.userInfo.userId)
+     if(response.status!==204){
+       this.$router.push("giveRating");
+     }
+
+   },
+ },*/
+}
+</script>
+
+<style scoped>
+.systemMessageContainer {
+  background-color: rgba(142, 202, 230, 0.5);
+  max-width: 280px;
+  width: 100%;
+  border-radius: 0 20px 20px 0;
+  padding: 15px 20px;
+  border-left: 3px solid #fb8500;
+  margin-bottom: 1rem;
+}
+#rateMessage {
+  font-size: 15px;
+  text-align: left;
+
+}
+#rateMessage label {
+  font-size: 14px;
+}
+#rateMessage svg {
+  margin-bottom: -5px;
+}
+</style>
