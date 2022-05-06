95f0d37547be28e036aaa7e12768ee3dfb4519e7
Ida Green Heglund <idagh@stud.ntnu.no>
2022-05-06 17:33:34 +0200

    fixed bugs and css filtersearch

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,12 @@
 <template>
   <HomeComponent></HomeComponent>
   <div class="itemsContainer">
-    <div class="itemsHeader">
-      <h3>Nyeste annonser</h3>
-      <p>Se de nyeste annonsene på boco</p>
+    <div id="infoText">
+      <div class="itemsHeader">
+        <h3>Nyeste annonser</h3>
+        <p>Se de nyeste annonsene på boco</p>
+      </div>
+        <p @click.stop="routeToAllItems" id="link">Se alle gjenstander</p>
     </div>
     <div class="items">
       <ItemCardSquare
@@ -56,6 +59,9 @@ export default {
       await this.$store.dispatch("setItem", item);
       await this.$router.push({ name: "ProductDetails" });
     },
+    async routeToAllItems() {
+      await this.$router.push({name: "SearchedItems"});
+    }
   },
   async created() {
     this.$store.dispatch("getItems", {
@@ -82,38 +88,14 @@ p {
   font-size: 14px;
   margin-bottom: 15px;
 }
-
-.header {
-  position: absolute;
+#infoText {
   display: flex;
-  width: 87%;
+  flex-direction: row;
   justify-content: space-between;
-  padding: 1rem 20px 1rem 20px;
 }
-.hamburgerMenu {
-  fill: white;
-  width: 2.5rem;
+#link {
+  color: var(--blue);
 }
-.hamburgerButton {
-  width: 2.5rem;
-  padding: 0;
-  background: none;
-  border: none;
-  position: relative;
-}
-.homeLink {
-  font-size: 40px;
-  background: transparent;
-  border: none;
-}
-.notificationIcon {
-  fill: white;
-  width: 2.5rem;
-}
-.logoImage {
-  width: 8rem;
-}
-
 .itemsHeader {
   text-align: left;
 }
